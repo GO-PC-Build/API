841658baed4497a0c7f24b751a5d69b04d357e69
Arthurdw <mail@arthurdw.com>
2020-12-28 14:00:50 +0100

    📝 Created todo note.

--- src/utils/auth.rs
+++ src/utils/auth.rs
@@ -2,7 +2,10 @@ use actix_web::HttpRequest;
 
 pub fn is_valid_request(req: &HttpRequest) -> bool {
     match req.headers().get("Authorization") {
-        Some(_) => true,
+        Some(token) => {
+            // TODO: Check in DB if its a valid token
+            true
+        },
         None => false
     }
 }
